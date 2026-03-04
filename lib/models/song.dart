34833476b487b4cb81aa871ec442092c908e0dea
DsChien102 <22010008@st.phenikaa-uni.edu.vn>
2026-03-04 21:31:27 +0700

    mô tả thay đổi

---
+++ lib/models/song.dart
@@ -0,0 +1,8 @@
+//khai báo thuộc tính trong class Song
+class Song {
+  final String title;
+  final String artist;
+  final String image;
+  //khi khởi tạo Song thì phải truyền đủ các giá trị của title, artist và image
+  Song({required this.title, required this.artist, required this.image});
+}
