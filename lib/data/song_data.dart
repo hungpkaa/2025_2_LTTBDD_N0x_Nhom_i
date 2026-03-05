3908d89a5748a691941594305daeeb3aa724770d
DsChien102 <22010008@st.phenikaa-uni.edu.vn>
2026-03-05 23:09:16 +0700

    thêm dữ liệu bài hát trong file song_data

---
+++ lib/data/song_data.dart
@@ -0,0 +1,27 @@
+import '../models/song.dart';
+
+List<Song> songs = [
+  Song(
+    title: "Chúng ta của tương lai ",
+    artist: "Sơn Tùng ",
+    image:
+        "C:\mobile\flutter_application_1\projectflutter\assets\images\maxresdefault.jpg",
+  ),
+  Song(
+    title: "Thiên Lý Ơi",
+    artist: "J97",
+    image:
+        "C:\mobile\flutter_application_1\projectflutter\assets\images\01-16.jpg",
+  ),
+  Song(
+    title: "Tháng Năm ",
+    artist: "Soobin",
+    image:
+        "C:\mobile\flutter_application_1\projectflutter\assets\images\photo_2020-12-15_22-13-45.jpg",
+  ),
+  Song(
+    title: "ABCD ",
+    artist: "Justinbeber",
+    image: "C:\mobile\flutter_application_1\projectflutter\assets\images\R.jpg",
+  ),
+];
