68c26844834b8a94ab15ee4a409f446adc296c27
DsChien102 <22010008@st.phenikaa-uni.edu.vn>
2026-03-06 22:36:32 +0700

    taoj file song_title hiển thị bài hát

--- lib/widgets/song_tile.dart
+++ lib/widgets/song_tile.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import '../models/song.dart';
 
+class SongTile extends StatelessWidget {
+  final Song song;
+  final VoidCallback onTap;
+
+  const SongTile({super.key, required this.song, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.asset(
+        song.image,
+        width: 50,
+        height: 50,
+        fit: BoxFit.cover,
+      ),
+      title: Text(song.title),
+      subtitle: Text(song.artist),
+      trailing: const Icon(Icons.play_arrow),
+      onTap: onTap,
+    );
+  }
+}
