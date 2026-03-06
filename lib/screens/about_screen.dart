e85e3d00e45ec568d8a704ac695111dfee9af8b0
DsChien102 <22010008@st.phenikaa-uni.edu.vn>
2026-03-06 22:57:07 +0700

    tạo trang thông tin nhóm

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("About Team")),
+      body: const Center(
+        child: Text(
+          "App nghe nhạc tạo bởi Hưng Vũ ",
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
