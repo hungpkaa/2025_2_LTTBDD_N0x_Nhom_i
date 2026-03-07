ee9468f96e98e1f1946638440b060e4c81594014
DsChien102 <22010008@st.phenikaa-uni.edu.vn>
2026-03-07 23:22:25 +0700

    tao trang home de co the chuyen sang cac trang giao dien khac

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import '../data/songs_data.dart';
+import '../widgets/song_tile.dart';
+import 'now_playing_screen.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Music App")),
+      body: ListView.builder(
+        itemCount: songs.length,
+        itemBuilder: (context, index) {
+          return SongTile(
+            song: songs[index],
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => PlayerScreen(song: songs[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
